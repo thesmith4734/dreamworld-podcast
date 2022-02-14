466575611f881f5a22d17c5c6d91fbf4d762095e
thesmith4734 <thehellhound4734@aim.com>
2022-02-13 23:21:51 -0800

    add s3 env vars

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.flaskenv'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-string-is-private'
@@ -8,3 +11,6 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SLEEPCAST_DIRECTORY = os.path.abspath(os.path.join(basedir, 'app/assets/sleepcasts'))
+
+    AWS_S3_KEY_ID = os.environ.get('AWS_S3_KEY_ID')
+    AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
